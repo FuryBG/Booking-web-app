883ea4a6b01add397b57497c6ffa8e257f768506
FuryBG <stoyanovbg@abv.bg>
2022-01-29 23:35:36 +0200

    views update

--- models/User.js
+++ models/User.js
@@ -2,7 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
     email: {type: String, required: true},
-    hashedPassword: {type: String, required: true}
+    username: {type: String, required: true},
+    hashedPassword: {type: String, required: true},
+    booked: [{type: Schema.Types.ObjectId, ref: "Hotel"}],
+    offer: [{type:Schema.Types.ObjectId, ref: "Hotel"}]
 });
 
 module.exports = model("User", schema);
